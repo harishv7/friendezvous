4700100f749703d1a203b34478487499edec0523
harishv7 <harishv207@gmail.com>
2015-05-25 15:25:59 +0800

    Updated CSS, content

--- js/application.js
+++ js/application.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
-    $('.row').on('click','button',function(){
+    $('.row').on('click','button',function(event){
+        event.preventDefault();
         $('body').find('.main-body').slideToggle();
         $(window).scrollTop($('.main-body').offset().top);
+         $("body, html").animate({ 
+            scrollTop: $( $(this).attr('href') ).offset().top 
+        }, 900);
     });
 });
