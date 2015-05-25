1244f93257cb12d4fb6337633277904c79dbd899
harishv7 <harishv207@gmail.com>
2015-05-25 15:35:17 +0800

    Updated all content and css

--- js/application.js
+++ js/application.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-    $('.row').on('click','button',function(){
-        $('body').find('.main-body').slideToggle();
-        $(window).scrollTop($('.main-body').offset().top);
-=======
     $('.row').on('click','button',function(event){
         event.preventDefault();
         $('body').find('.main-body').slideToggle();
@@ -11,6 +6,5 @@ $(document).ready(function() {
          $("body, html").animate({ 
             scrollTop: $( $(this).attr('href') ).offset().top 
         }, 900);
->>>>>>> master
     });
 });
