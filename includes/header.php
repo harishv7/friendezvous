a95019f066894706b40746b9e335cf92c3eadef2
harishv7 <harishv207@gmail.com>
2015-06-03 00:46:42 +0800

    Fixed bootstrap linkage

--- includes/header.php
+++ includes/header.php
@@ -8,13 +8,15 @@
 
 	<!-- CSS FILES -->
 	<link type="text/css" href="/assets/css/font-awesome.min.css" rel="stylesheet">
+    <link type="text/css" href="/assets/css/bootstrap.min.css" rel="stylesheet">
 	<link type="text/css" href="/assets/css/main.css" rel="stylesheet">
 	<link type="text/css" href="/assets/css/hover-min.css" rel="stylesheet" media="all">
 	<link type="text/css" href="/assets/css/mycss.css" rel="stylesheet">
 
 	<!-- SCRIPTS -->
+    <script type="text/javascript" src="/assets/js/jquery.min.js"></script>
 	<script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="/assets/js/application.js"></script>
-	<script type="text/javascript" src="/assets/js/jquery.min.js"></script>
+
 
 </head>
