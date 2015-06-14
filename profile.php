bcc127ef8299d97e602f01e7c5a5923f77a8f9da
Sn1p3r5 <alvian.better@gmail.com>
2015-06-14 21:58:38 +0800

    Profile page includes fix
    - Fixed PHP includes in profile.php (alleviate errors)

--- profile.php
+++ profile.php
@@ -2,12 +2,12 @@
 <html>
     
     <?php
-		include 'head.php';
+		include 'includes/header.php';
 	?>
     
     <body>
         <?php
-			include 'navigationBar.php';
+			include 'includes/navigationBar.php';
 		?>
 		
 		<div class="section">
@@ -27,7 +27,7 @@
 		</div>
 		
 		<?php
-			include 'footer.php';
+			include 'includes/footer.php';
 		?>
 	</body>
 	
