b6654a6f4cbd2ecc7de3f75ca828eeea1c275930
Sn1p3r5 <alvian.better@gmail.com>
2015-06-27 22:42:54 +0800

    Removed unused files

---
+++ ajax_refresh-users-as.php
@@ -0,0 +1,21 @@
+<?php
+	include 'includes/session.php';
+	// PDO connect *********
+	function connect() {
+		return new PDO('mysql:host=fdb7.awardspace.net;dbname=1895198_frndzvs', '1895198_frndzvs', 'friendezvousorbital2015', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES latin1"));
+	}
+
+	$pdo = connect();
+	$keyword = '%'.$_POST['keyword'].'%';
+	$sql = "SELECT * FROM users WHERE full_name LIKE (:keyword) && user_id != $user_id ORDER BY user_id ASC LIMIT 0, 10";
+	$query = $pdo->prepare($sql);
+	$query->bindParam(':keyword', $keyword, PDO::PARAM_STR);
+	$query->execute();
+	$list = $query->fetchAll();
+	foreach ($list as $rs) {
+		// put in bold the written text
+		$full_name = str_replace($_POST['keyword'], '<b>'.$_POST['keyword'].'</b>', $rs['full_name']);
+		// add new option
+		echo '<li onclick="set_item(\''.str_replace("'", "\'", $rs['email']).'\')">'.$full_name.'<br><font color=#808080>&nbsp'.$rs['email'].'</font></li>';
+	}
+?>
