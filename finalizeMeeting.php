2c850034024ac6224eb88f0c35ebf78f8be17c8b
Sn1p3r5 <alvian.better@gmail.com>
2015-07-22 20:33:15 +0800

    Finalized meetings now send notifications to all participants

--- finalizeMeeting.php
+++ finalizeMeeting.php
@@ -6,7 +6,19 @@
 	$date_time = mysqli_real_escape_string($connection, $_GET['date_time']);
 	
 	$query = "UPDATE meetings SET finalized=1, date_time='$date_time' WHERE meeting_id=$meeting_id";
-	$result = mysqli_query($connection, $query) OR DIE(mysqli_error($connection));
+	$result = mysqli_query($connection, $query);
+	
+	$query = "SELECT * FROM meetings WHERE meeting_id=$meeting_id";
+	$result = mysqli_query($connection, $query);
+	$meeting = mysqli_fetch_array($result);
+	
+	$query = "SELECT * FROM meeting_users WHERE meeting_id=$meeting_id";
+	$result = mysqli_query($connection, $query);
+	while ($participant = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+		$participant_id = $participant['user_id'];
+		$query = "INSERT INTO notifications (user_id, notification_message, notification_link) VALUES ('$participant_id', 'Meeting $meeting[name] has been finalized.', 'meeting.php?meeting_id=$meeting_id')";
+		$result = mysqli_query($connection, $query);
+	}
 	
 	header("Location: meeting.php?meeting_id=$meeting_id");
 ?>
