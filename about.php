4db87985a722845068e0a8b4c7b251807be98209
harishv7 <harishv207@gmail.com>
2015-08-01 14:27:07 +0800

    Replaced image (fast load)

--- about.php
+++ about.php
@@ -18,7 +18,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <img src="assets/images/mockup.png" class="img-responsive">
+                        <img src="https://unsplash.it/800/500?image=0" class="img-responsive">
 					</div>
                     <div class="col-md-6">
                         <h1>Friendezvous</h1>
