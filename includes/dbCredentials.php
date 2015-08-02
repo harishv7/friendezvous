3fa5a4d968b55e5936612510547193d561bb5155
Sn1p3r5 <alvian.better@gmail.com>
2015-08-02 21:33:48 +0800

    dbCredentials

--- includes/dbCredentials.php
+++ includes/dbCredentials.php
@@ -1,6 +1,8 @@
 <?php
-	$db_host = 'localhost';
-	$db_user = 'root';
-	$db_pass = '';
-	$db_name = 'friendezvous';
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+	$db_host = $url["host"];
+	$db_user = $url["user"];
+	$db_pass = $url["pass"];
+	$db_name = substr($url["path"], 1);
 ?>
