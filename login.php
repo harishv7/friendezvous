9fba5af7c273efd788655d52ad752c98b2fd3b26
Sn1p3r5 <alvian.better@gmail.com>
2015-08-02 22:21:22 +0800

    Minor change

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 
 	$helper = $fb->getRedirectLoginHelper();
 	$permissions = ['email', 'public_profile']; // optional
-	//$loginUrl = $helper->getLoginUrl('http://polar-gorge-5952.herokuapp.com/login-callback.php', $permissions);
+	$loginUrl = $helper->getLoginUrl('http://polar-gorge-5952.herokuapp.com/login-callback.php', $permissions);
 ?>
 <!DOCTYPE html>
 <html>
