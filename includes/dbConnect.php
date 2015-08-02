68d2b3529c6caecfcd172c6f8f0b027458674ccb
Sn1p3r5 <alvian.better@gmail.com>
2015-08-02 21:37:06 +0800

    dbConnect update

--- includes/dbConnect.php
+++ includes/dbConnect.php
@@ -1,5 +1,5 @@
 <?php
 	include 'dbCredentials.php';
 	
-	$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die(mysqli_error($connection));
+	$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 ?>
