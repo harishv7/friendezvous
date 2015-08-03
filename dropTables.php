5eaa06f440e9d29b51f86b5704cf3f25de0871e1
Sn1p3r5 <alvian.better@gmail.com>
2015-08-03 19:49:27 +0800

    Bug fixes

--- dropTables.php
+++ dropTables.php
@@ -1,6 +1,13 @@
 <?php
 	include 'includes/session.php';
 	include 'includes/dbConnect.php';
+
+	$query = "SELECT * FROM users WHERE user_id='$user_id'";
+	$result = mysqli_query($connection, $query);
+	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+	if (!$result || $row['access_level'] < 2){
+		header("Location: error.php");
+	}
 	
 	$query = "DROP TABLE users";
 	$result1 = mysqli_query($connection, $query);
@@ -29,10 +36,5 @@
 	$query = "DROP TABLE notifications";
 	$result9 = mysqli_query($connection, $query);
 	
-	if ($result1 && $result2 && $result3 && $result4 && $result5 && $result6 && $result7 && $result8 && $result9){
-		echo "Dropped all tables succesfully";
-	}
-	else {
-		echo "Failed to drop one or more table(s)."
-	}
+	echo "Dropped all tables succesfully";
 ?>
