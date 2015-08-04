abd810d7b63083808ef5cb9d43b26db14f577525
harishv7 <harishv207@gmail.com>
2015-08-04 19:30:05 +0800

    update header

--- includes/header.php
+++ includes/header.php
@@ -11,10 +11,10 @@
 	<link type="text/css" href="assets/css/main.css" rel="stylesheet">
 	<link type="text/css" href="assets/css/hover-min.css" rel="stylesheet" media="all">
 	<link type="text/css" href="assets/css/mycss.css" rel="stylesheet">
-	<link type='text/css' href="assets/css/google-fonts.css" rel="stylesheet">
+    <link type='text/css' href="assets/css/google-fonts.css" rel="stylesheet">
 	<link type='text/css' href="assets/css/jquery-ui.min.css" rel="stylesheet">
-	<link type='text/css' href="assets/css/jquery-ui.structure.min.css" rel="stylesheet">
-	<link type='text/css' href="assets/css/jquery-ui.theme.min.css" rel="stylesheet">
+    <link type='text/css' href="assets/css/jquery-ui.structure.min.css" rel="stylesheet">
+    <link type='text/css' href="assets/css/jquery-ui.theme.min.css" rel="stylesheet">
 	<link type='text/css' href="assets/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
 	<link type='text/css' href="assets/css/autocomplete.css" rel="stylesheet">
 	<link type='text/css' href="assets/css/maps.css" rel="stylesheet">
@@ -25,32 +25,9 @@
 	<script type="text/javascript" src="assets/js/application.js"></script>
 	<script type="text/javascript" src="assets/js/jquery.waypoints.min.js"></script>
 	<script type="text/javascript" src="assets/js/bday-picker.min.js"></script>
-	<script type="text/javascript" src="assets/js/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="assets/js/dashboard.js"></script>
+    <script type="text/javascript" src="assets/js/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="assets/js/dashboard.js"></script>
 	<script type="text/javascript" src="assets/js/moment.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap-datetimepicker.min.js"></script>
 	<script type="text/javascript" src="assets/js/autocomplete-users.js"></script>
-
-	<!-- FAVICONS -->
-	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="assets/favicons/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/favicons/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/favicons/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/favicons/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="assets/favicons/apple-touch-icon-60x60.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="assets/favicons/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="assets/favicons/apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="assets/favicons/apple-touch-icon-152x152.png" />
-	<link rel="icon" type="image/png" href="assets/favicons/favicon-196x196.png" sizes="196x196" />
-	<link rel="icon" type="image/png" href="assets/favicons/favicon-96x96.png" sizes="96x96" />
-	<link rel="icon" type="image/png" href="assets/favicons/favicon-32x32.png" sizes="32x32" />
-	<link rel="icon" type="image/png" href="assets/favicons/favicon-16x16.png" sizes="16x16" />
-	<link rel="icon" type="image/png" href="assets/favicons/favicon-128.png" sizes="128x128" />
-	<meta name="application-name" content="&nbsp;"/>
-	<meta name="msapplication-TileColor" content="#FFFFFF" />
-	<meta name="msapplication-TileImage" content="assets/favicons/mstile-144x144.png" />
-	<meta name="msapplication-square70x70logo" content="assets/favicons/mstile-70x70.png" />
-	<meta name="msapplication-square150x150logo" content="assets/favicons/mstile-150x150.png" />
-	<meta name="msapplication-wide310x150logo" content="assets/favicons/mstile-310x150.png" />
-	<meta name="msapplication-square310x310logo" content="assets/favicons/mstile-310x310.png" />
-
 </head>
